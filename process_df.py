6d281df1d9ca9f34857f9559c354eec8fa8a57de
gitgup <gups.singhs@gmail.com>
2023-04-16 22:12:43 -0700

    added save, load, remove_duplicates methods

--- process_df.py
+++ process_df.py
@@ -1,5 +1,7 @@
 import uproot
 from common import np, pd
+import pickle
+import os
 
 #removes empty columns given a data frame: returns data frame
 def remove_empty(df):
@@ -13,3 +15,26 @@ def divide_by_1000(x):
     x_arr = np.array(x)
     x_arr /= 1000
     return uproot.STLVector(x_arr.tolist())
+
+# Save data frame using pickle library (saves to folder named pickled in same dir)
+def save(df, name):
+    if not os.path.exists('pickled'):
+        os.makedirs('pickled')
+    with open('pickled/{}.pickle'.format(name), 'wb') as f:
+        pickle.dump(df, f)
+    
+# Read data frame and load it
+def load(name):
+    if not os.path.exists('pickled'):
+        os.makedirs('pickled')
+    with open('pickled/{}.pickle'.format(name), 'rb') as f:
+        df = pickle.load(f)
+    return df
+
+#if there are more than 2 particles in an event, remove the event (for single particle gun sampels)
+def remove_duplicates(df):
+    indicies = [i for i, x in enumerate(df[df.columns[0]]) if len(x) > 2]
+    df = df.drop(indicies)
+    df = df.reset_index(drop=True)
+    print("Removed {}".format(len(indicies)) + " duplicate particles")
+    return df
